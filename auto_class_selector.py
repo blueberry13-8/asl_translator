8716cfe0402744058a0a444747a0e711fef79d28
Nagim123 <fourcolorsgame@gmail.com>
2023-06-15 23:06:18 +0300

    Add class selector from dataset

---
+++ auto_class_selector.py
@@ -0,0 +1,30 @@
+import json
+
+
+classes_to_extract = ["help", "love", "hello", "people", "thank you", "remember"]
+
+class_file = open("wlasl_class_list.txt", "r")
+class_data = class_file.read().split("\n")
+class_file.close()
+
+classes_indexes = []
+
+for i in range(0, len(class_data)-1):
+    line = class_data[i]
+    pair = line.strip().split('\t')
+    index = int(pair[0])
+    value = pair[1]
+    if value in classes_to_extract:
+        classes_indexes.append(index)
+
+videos = json.load(open("nslt_2000.json"))
+
+new_data_file = {}
+
+for key in videos:
+    if int(videos[key]["action"][0]) in classes_indexes:
+        new_data_file[key] = videos[key]
+
+result_file = open("output.json", "w")
+result_file.write(json.dumps(new_data_file))
+result_file.close()
